767c7b1015fe5354e4f02b920c62eb48b7ce9409
ilikecubesnstuff <subho1618@gmail.com>
2021-10-23 19:34:44 +0100

    Locks without classes because classes are disgusting.

--- bot.py
+++ bot.py
@@ -13,74 +13,9 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("discord")
 logger.setLevel(logging.INFO)
 
-# Shared resources that require locks.
-class BanCache(set):
-    """
-    Set of users recently banned for fraud.
-    Shared resource intended to be used via acquiring its lock:
-
-    async with BanCache.lock:
-        # use BanCache object
-    """
-    lock = asyncio.Lock()
-
-class Officer:
-    """
-    Routine for notifying a channel and banning a user.
-    Shared resource intended to be used via acquiring its lock:
-
-    async with Officer.lock:
-        # use Officer object
-    """
-    lock = asyncio.Lock()
-
-    @staticmethod
-    async def activate(message, instance):
-        """
-        Notify a channel, and if enabled, ban the message author.
-        :param message: The created message.
-        :param instance: The scam link found in the message's contents.
-        """
-        # Check if user has already been banned.
-        async with client.ban_cache.lock:
-            if message.author.id in client.ban_cache:
-                logging.debug(f"User {message.author.id} found in ban cache.")
-                return
-
-        # Look up the channel to notify.
-        channel = client.get_channel(int(bot["channel"]))
-        if channel is None:
-            logging.error("Could not log as channel is invalid.")
-            return
-
-        # Notify the channel with a rich embed.
-        user_id = message.author.id
-        link = f"[{instance}]({instance})"
-        description = f"Automatically banned **{user_id}** ({message.author.name}#{message.author.discriminator}) for the following link. Please verify:\n{link}"
-        embed = discord.Embed(description=description, colour=0xff0000)
-        await channel.send(embed=embed)
-
-        # If set up, ban the user.
-        if bot["ban"]:
-            # First, DM the user about the ban.
-            dm = bot["dm"]
-            if dm != "":
-                # Attempt to send DM, if it fails, not the end of the world.
-                try:
-                    await message.author.send(dm)
-                except (discord.HTTPException, discord.Forbidden):
-                    logging.error(f"Private messaging {user_id} was not successful")
-
-            # Attempt to ban.
-            try:
-                await message.author.ban(reason="Fraud.", delete_message_days=1)
-                async with client.ban_cache.lock:
-                    client.ban_cache.add(message.author.id)
-                    logging.debug(f"User {message.author.id} cached.")
-                return 1
-            except (discord.HTTPException, discord.Forbidden) as e:
-                logging.error(f"Banning {user_id} was not successful")
-
+# Create locks for shared resources.
+ban_cache_lock = asyncio.Lock() # used for "client.ban_cache"
+officer_lock = asyncio.Lock() # used for "activate_officer" function
 
 # We just need guild updates (to see channels and whatnot) and messages.
 intents = discord.Intents(guilds=True, messages=True)
@@ -92,9 +27,6 @@ client = discord.Client(
     guild_subscriptions=False,
     member_cache_flags=discord.MemberCacheFlags().none()
 )
-client.ban_cache = BanCache()
-client.officer = Officer()
-
 # URL regular expression precompiled for performance (from the internet).
 url_pattern = re.compile(r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))", re.DOTALL)
 
@@ -109,6 +41,7 @@ async def on_ready():
     """
     Print out when the bot is ready.
     """
+    client.ban_cache = set()
     logging.info("Andy awake and monitoring for fraud.")
 
 
@@ -156,15 +89,61 @@ async def on_message(message):
 
     # If there has been a scam, deal with it.
     if scam:
-        async with client.officer.lock:
-            ban_success = await client.officer.activate(message, instance)
+        async with officer_lock:
+            ban_success = await activate_officer(message, instance)
         if ban_success:
             await asyncio.sleep(60)
-            async with client.ban_cache.lock:
+            async with ban_cache_lock:
                 client.ban_cache.remove(message.author.id)
             logging.debug(f"User {message.author.id} removed from ban cache.")
 
 
+async def activate_officer(message, instance):
+    """
+    Notify a channel, and if enabled, ban the message author.
+    :param message: The created message.
+    :param instance: The scam link found in the message's contents.
+    """
+    # Check if user has already been banned.
+    async with ban_cache_lock:
+        if message.author.id in client.ban_cache:
+            return
+
+    # Look up the channel to notify.
+    channel = client.get_channel(int(bot["channel"]))
+    if channel is None:
+        logging.error("Could not log as channel is invalid.")
+        return
+
+    # Notify the channel with a rich embed.
+    user_id = message.author.id
+    link = f"[{instance}]({instance})"
+    description = f"Automatically banned **{user_id}** ({message.author.name}#{message.author.discriminator}) for the following link. Please verify:\n{link}"
+    embed = discord.Embed(description=description, colour=0xff0000)
+    await channel.send(embed=embed)
+
+    # If set up, ban the user.
+    if bot["ban"]:
+        # First, DM the user about the ban.
+        dm = bot["dm"]
+        if dm != "":
+            # Attempt to send DM, if it fails, not the end of the world.
+            try:
+                await message.author.send(dm)
+            except (discord.HTTPException, discord.Forbidden):
+                logging.error(f"Private messaging {user_id} was not successful")
+
+        # Attempt to ban.
+        try:
+            await message.author.ban(reason="Fraud.", delete_message_days=1)
+            # async with ban_cache_lock:
+            #     client.ban_cache.add(message.author.id)
+            # return 1
+        except (discord.HTTPException, discord.Forbidden) as e:
+            logging.error(f"Banning {user_id} was not successful")
+        async with ban_cache_lock:
+            client.ban_cache.add(message.author.id)
+        return 1
 
 # Run the bot.
 client.run(bot["token"])
